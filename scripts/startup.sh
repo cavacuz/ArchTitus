78447ec98fa03075bc0585db4323367902bd5564
cavacuz <cavacuz@gmail.com>
2024-03-22 14:17:46 +0000

    Update startup.sh
    added pt-latin1 keyboard layout

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -249,7 +249,7 @@ keymap () {
 echo -ne "
 Please select key board layout from this list"
 # These are default key maps as presented in official arch repo archinstall
-options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
+options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl pt-latin1 ro ru sg ua uk)
 
 select_option $? 4 "${options[@]}"
 keymap=${options[$?]}
